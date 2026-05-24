89a8fcb6204d530776e344a3105548130f435a60
Massayoshii <Massayoshibando@gmail.com>
2026-05-24 10:39:13 -0300

    Controller Criado

---
+++ crud/src/main/java/controller/ProdutoController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import entity.Produto;
+import org.springframework.web.bind.annotation.*;
+import service.ProdutoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        return service.salvar(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar(){
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable Long id){
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id,
+                             @RequestBody Produto produto){
+        return service.atualizar(id,produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        service.deletar(id);
+    }
+}
